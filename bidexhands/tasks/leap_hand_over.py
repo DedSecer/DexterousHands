ef9e863ec4b4f868654986924ce7bce580da0cf3
DedSecer <z2465216809qq.com@icloud.com>
2025-05-04 11:29:12 +0800

    object pose

--- bidexhands/tasks/leap_hand_over.py
+++ bidexhands/tasks/leap_hand_over.py
@@ -292,17 +292,15 @@ class LeapHandOver(BaseTask):
         allegro_hand_start_pose = gymapi.Transform()
         allegro_hand_start_pose.p = gymapi.Vec3(*get_axis_params(0.5, self.up_axis_idx))
         allegro_hand_start_pose.r = gymapi.Quat().from_euler_zyx(1.571, -1.571, 0)
-        allegro_hand_start_pose.r *= gymapi.Quat().from_euler_zyx(0, -0.4, 0)
 
         allegro_another_hand_start_pose = gymapi.Transform()
         allegro_another_hand_start_pose.p = gymapi.Vec3(0, -0.7, 0.5)
         allegro_another_hand_start_pose.r = gymapi.Quat().from_euler_zyx(-1.571, -1.571, 0)
-        allegro_another_hand_start_pose.r *= gymapi.Quat().from_euler_zyx(0, -0.4, 0)
 
         object_start_pose = gymapi.Transform()
         object_start_pose.p = gymapi.Vec3()
         object_start_pose.p.x = allegro_hand_start_pose.p.x
-        pose_dy, pose_dz = -0.1, 0.06
+        pose_dy, pose_dz = -0.2, 0.06
 
         object_start_pose.p.y = allegro_hand_start_pose.p.y + pose_dy
         object_start_pose.p.z = allegro_hand_start_pose.p.z + pose_dz
@@ -310,7 +308,7 @@ class LeapHandOver(BaseTask):
         if self.object_type == "pen":
             object_start_pose.p.z = allegro_hand_start_pose.p.z + 0.02
 
-        self.goal_displacement = gymapi.Vec3(0., -0.18, -0.05)
+        self.goal_displacement = gymapi.Vec3(0., -0.08, 0)
         self.goal_displacement_tensor = to_torch(
             [self.goal_displacement.x, self.goal_displacement.y, self.goal_displacement.z], device=self.device)
         goal_start_pose = gymapi.Transform()
